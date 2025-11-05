382b6ffede2ba747b68bcba197ef4fc126805f47
KenzoSenna <kenzoandblade@gmail.com>
2025-11-05 11:45:39 -0400

    feat: implement CharactersController with CRUD methods and update routes

--- ApiLol/app/controllers/characters_controller.ts
+++ ApiLol/app/controllers/characters_controller.ts
@@ -1,6 +1,27 @@
 // import type { HttpContext } from '@adonisjs/core/http'
 
+import Character from "#models/character";
+
 export default class CharactersController {
 
+    async index(){
+        const characters = await Character.query()
+        return characters
+    }
 
+    async store(){
+        
+    }
+
+    async show(){
+
+    }
+
+    async update(){
+
+    }
+
+    async destroy(){
+        
+    }
 }
--- ApiLol/start/routes.ts
+++ ApiLol/start/routes.ts
@@ -6,11 +6,10 @@
 | The routes file is used for defining the HTTP routes.
 |
 */
-
+const UsersController = () => import('#controllers/users_controller')
+import CharactersController from '#controllers/characters_controller'
 import router from '@adonisjs/core/services/router'
+// import { middleware } from './kernel.js'
 
-router.get('/', async () => {
-  return {
-    hello: 'world',
-  }
-})
+router.resource('user', UsersController).apiOnly()
+router.resource('characters', CharactersController).apiOnly()
